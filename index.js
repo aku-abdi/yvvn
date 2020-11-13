05c9fbf1d39456bb73d27838756dac4b23b58bff
aku-abdi <74408765+aku-abdi@users.noreply.github.com>
2020-11-13 19:38:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'YwNBoT'; 
+const instagram = 'https://www.instagram.com/yuanabdillah/'; 
+const telegram = 'https://t.me/ywnabdillah'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const whatsapp = '+6285757122558';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
